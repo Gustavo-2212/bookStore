89e7f601e3a2aefc9a228840b15753c302b71a0d
Gustavo Alves <gustavoalvesdeoliveira07@gmail.com>
2022-03-25 21:39:03 -0300

    update

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+// cd /cmd/main/ - go build - go run main.go
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Gustavo-2212/bookStore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
